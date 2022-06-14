e347d4eba17ad8aa47cc5ddccb219af032ec9d91
Krasilnikov-Ilya <fatum0889@mail.ru>
2022-06-14 20:45:44 +0400

    API tested with Junit5 and Apache Http Client5

--- src/test/java/apachehttpclient5/ApacheHttp5.java
+++ src/test/java/apachehttpclient5/ApacheHttp5.java
@@ -1,5 +1,6 @@
 package apachehttpclient5;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -14,6 +15,12 @@ import java.io.IOException;
 public class ApacheHttp5 {
     public static CloseableHttpClient createClient() { // создание клиента
         return HttpClients.createDefault();
+    }
+
+    public static HttpGet createHttpGetWithHeader() { // создание запроса без билдера
+        HttpGet request = new HttpGet("http://77.50.236.203:4880/users");
+        request.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        return request;
     }
 
     public static ClassicHttpRequest createGetWithHeader() { // создание запроса
