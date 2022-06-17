26f2841a34dd614f2dd05eeb096d2b4b08a34d9a
Krasilnikov-Ilya <fatum0889@mail.ru>
2022-06-17 20:00:53 +0400

    half-cleaned

--- src/main/java/utils/JDBCRecorder.java
+++ src/main/java/utils/JDBCRecorder.java
@@ -21,25 +21,20 @@ public class JDBCRecorder {
             //Создаю соединение
             connection = DriverManager.getConnection(url, name, password);
             //инициализирую результат
-            Statement statement = null;
+            Statement statement;
             //заполняю результат
             statement = connection.createStatement();
             ResultSet result1 = statement.executeQuery(
                     "SELECT * FROM person");
             //result это указатель на первую строку с выборки
             //метод next() переходит к следующему элементу в выборке
-            //System.out.println("Recording statement");
             List<User> usersPageRootSQL = new ArrayList<>();
             while (result1.next()) {
                 User user = new User(result1.getInt("id"), result1.getString("first_name"),result1.getString("second_name"),
                         result1.getInt("age"),(result1.getBoolean("sex") ? "MALE" : "FEMALE"),result1.getDouble("money"));
                 usersPageRootSQL.add(user);
-                //System.out.println(user.toString());
             }
-            //System.out.println(usersPageRootSQL.toString());
             return usersPageRootSQL;
-
-
 
         } catch (Exception ex) {
             //выводим наиболее значимые сообщения
